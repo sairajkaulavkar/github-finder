96d9855e2b21f1a512f8b79138bc242d39ca56ac
Sairaj Kaulavkar <58000315+sairajkaulavkar@users.noreply.github.com>
2021-03-04 23:22:23 +0530

    Create app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+//guarding
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin=()=> redirectUnauthorizedTo(['signIn']);
+const redirectLoggedInToHome=()=> redirectLoggedInTo(['']);
+const routes: Routes = [
+  {
+    path: 'signIn',
+    component: SigninComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
+  },
+  {
+    path: 'signUp',
+    component: SignupComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: '**',
+    component: PagenotfoundComponent,
+  },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
